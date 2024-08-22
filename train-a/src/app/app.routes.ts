f6f315f9aca1f1eebff698068fc9773adb284666
AngelinaBz <147927216+AngelinaBz@users.noreply.github.com>
2024-08-22 21:17:46 +0300

    Signup page (#8)
    * feat: create form for signup
    
    * feat: add repeat password field
    
    * refactor: add angular material for signup form
    
    * feat: add signup service
    
    * fix: fix lint
    
    * fix: fix service
    
    * fix: fix eslint config

--- train-a/src/app/app.routes.ts
+++ train-a/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { SignupComponent } from './auth/pages/signup/signup.component';
+
+export const routes: Routes = [{ path: 'signup', component: SignupComponent }];
