6ed8b2ebb16e3f2de067012c12b2eb0affc5a425
Vitali Kuntsevich <kvadesstudio@gmail.com>
2024-08-22 12:09:18 +0300

    fix: lint debug

--- train-a/src/app/app.component.ts
+++ train-a/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { HeaderComponent } from './core/components/header/header.component';
 
 @Component({
