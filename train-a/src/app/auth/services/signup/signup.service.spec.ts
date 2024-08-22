a400c6ac333d8533bc4ddf580e35f2621279eee2
FYM <34478249+FYMG@users.noreply.github.com>
2024-08-22 22:42:09 +0300

    Setup linters and ci (#7)
    * chore: setup ci
    
    * chore: update ci
    
    * chore: set node version
    
    * chore: set node version
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * chore: update ci
    
    * feat: setup ngrx and make linting better
    
    * fix: add provider to tests

--- train-a/src/app/auth/services/signup/signup.service.spec.ts
+++ train-a/src/app/auth/services/signup/signup.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { providers } from '../../../app.config';
 import { SignupService } from './signup.service';
 
 describe('SignupService', () => {
   let service: SignupService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers,
+    });
     service = TestBed.inject(SignupService);
   });
 
