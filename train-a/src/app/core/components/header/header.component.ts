8e656e21585ef288a0b8b656a199625d1d6bd0a0
Vitali Kuntsevich <kvadesstudio@gmail.com>
2024-08-22 15:26:37 +0300

    feat: add logic header link

--- train-a/src/app/core/components/header/header.component.ts
+++ train-a/src/app/core/components/header/header.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isAuthenticated = false;
+  isAdmin = false;
+
+  showSignupForm = false;
+  showLoginForm = false;
+
+  toggleSignup() {
+    this.showSignupForm = !this.showSignupForm;
+    this.showLoginForm = false;
+  }
+
+  toggleLogin() {
+    this.showLoginForm = !this.showLoginForm;
+    this.showSignupForm = false;
+  }
+
+  logout() {
+  }
+}
