3adc046537ec0b3dec9ade5e762544b37e7652ec
Angelina Bezik <angelina1337bz@gmail.com>
2024-08-23 18:35:36 +0300

    fix: fix test

--- train-a/src/app/auth/services/signin/signin.service.spec.ts
+++ train-a/src/app/auth/services/signin/signin.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { providers } from '../../../app.config';
 import { SigninService } from './signin.service';
 
 describe('SigninService', () => {
   let service: SigninService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers,
+    });
     service = TestBed.inject(SigninService);
   });
 
