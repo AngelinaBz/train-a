cd3c79d8902c23792efd50f8211061984b1419a2
MaruyYak <maryana.lus@gmail.com>
2024-08-26 17:25:14 +0300

    feat: add tests coverage

--- train-a/src/app/admin/pages/admin/admin.component.spec.ts
+++ train-a/src/app/admin/pages/admin/admin.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 import { AdminComponent } from './admin.component';
 
@@ -9,6 +11,14 @@ describe('AdminComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AdminComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of(new Map()),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AdminComponent);
