1563032129f07e6a8f7708392bff53a652bf281d
Angelina Bezik <angelina1337bz@gmail.com>
2024-08-30 14:31:05 +0300

    fix: fix lint

--- train-a/src/app/app.config.ts
+++ train-a/src/app/app.config.ts
@@ -14,10 +14,10 @@ import { CarriageEffects } from './carriages/state/carriage.effects';
 import { carriageReducer } from './carriages/state/carriage.reducers';
 import { OrderEffects } from './orders/state/order.effects';
 import { orderReducer } from './orders/state/order.reducers';
-import { StationEffects } from './stations/state/station.effects';
-import { stationReducer } from './stations/state/station.reducers';
 import * as detailsEffects from './search/state/details/details.effects';
 import { detailsReducer } from './search/state/details/details.reducers';
+import { StationEffects } from './stations/state/station.effects';
+import { stationReducer } from './stations/state/station.reducers';
 import * as userEffects from './user/state/user.effects';
 import { userReducer } from './user/state/user.reducers';
 
