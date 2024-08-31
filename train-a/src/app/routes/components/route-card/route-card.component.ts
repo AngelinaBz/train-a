efe0c2543aa86c02ae2768fc9964081e433fa350
Angelina Bezik <angelina1337bz@gmail.com>
2024-08-31 22:02:11 +0300

    feat: display all routes

--- train-a/src/app/routes/components/route-card/route-card.component.ts
+++ train-a/src/app/routes/components/route-card/route-card.component.ts
@@ -1,14 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
+import { CarriageFacade } from '../../../carriages/state/carriage.facade';
+import { Carriage } from '../../../carriages/state/carriage.model';
+import { Station } from '../../../stations/models/station.model';
+import { StationFacade } from '../../../stations/state/station.facade';
 import { Route } from '../../models/route.model';
 
 @Component({
   selector: 'app-route-card',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './route-card.component.html',
   styleUrl: './route-card.component.scss',
 })
-export class RouteCardComponent {
+export class RouteCardComponent implements OnInit {
   @Input() route!: Route;
+
+  stations: Station[] = [];
+  carriages: Carriage[] = [];
+
+  constructor(
+    private stationFacade: StationFacade,
+    private carriageFacade: CarriageFacade,
+  ) {}
+
+  ngOnInit() {
+    this.loadStations();
+    this.loadCarriages();
+  }
+
+  loadStations() {
+    this.stationFacade.stations$.subscribe((stations) => {
+      this.stations = stations.filter((station) => this.route.path.includes(station.id));
+    });
+  }
+
+  loadCarriages() {
+    this.carriageFacade.carriages$.subscribe((carriages) => {
+      this.carriages = carriages.filter((carriage) => this.route.carriages.includes(carriage.code));
+    });
+  }
 }
--- train-a/src/app/routes/pages/routes-admin-page/routes-admin-page.component.ts
+++ train-a/src/app/routes/pages/routes-admin-page/routes-admin-page.component.ts
@@ -1,21 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { MatToolbar } from '@angular/material/toolbar';
 
+import { CarriageFacade } from '../../../carriages/state/carriage.facade';
+import { StationFacade } from '../../../stations/state/station.facade';
 import { RouteCardComponent } from '../../components/route-card/route-card.component';
 import { RoutesFacade } from '../../state/routes.facade';
 
 @Component({
   selector: 'app-routes-admin-page',
   standalone: true,
-  imports: [MatToolbar, RouteCardComponent, CommonModule],
+  imports: [RouteCardComponent, CommonModule],
   templateUrl: './routes-admin-page.component.html',
   styleUrl: './routes-admin-page.component.scss',
 })
 export class RoutesAdminPageComponent implements OnInit {
   routes$ = this.routesFacade.routes$;
 
-  constructor(private routesFacade: RoutesFacade) {}
+  constructor(
+    private routesFacade: RoutesFacade,
+    private stationFacade: StationFacade,
+    private carriageFacade: CarriageFacade,
+  ) {}
 
   ngOnInit() {
     this.routesFacade.loadRoutes();
@@ -24,5 +29,7 @@ export class RoutesAdminPageComponent implements OnInit {
         console.log(routes);
       }
     });
+    this.carriageFacade.loadCarriages();
+    this.stationFacade.loadStations();
   }
 }
