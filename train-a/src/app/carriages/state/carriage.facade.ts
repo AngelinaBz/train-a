68e9358636337e8fc777cc8816d7c11577652d3a
MaruyYak <141480602+MaruyYak@users.noreply.github.com>
2024-09-02 00:59:18 +0300

    Feat/carriages page (#34)
    * feat: featch and display allcarieges
    
    * feat: add create carriage form
    
    * feat: create carriage functionality
    
    * feat: update carriage functionality
    
    * feat: add delete carriage functionality
    
    * feat: the correct order of the carriages
    
    * feat: add cariages validations
    
    * feat: add edit and select mode for carriage
    
    * feat: add carriage code to selected seats
    
    * feat: add carriage code to selected seats
    
    * fix: default values in form
    
    * fix: responsive styles
    
    ---------
    
    Co-authored-by: MaruyYak <maryana.lus@gmail.com>

--- train-a/src/app/carriages/state/carriage.facade.ts
+++ train-a/src/app/carriages/state/carriage.facade.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import * as CarriageActions from './carriage.actions';
+import { Carriage } from './carriage.model';
 import * as CarriageSelectors from './carriage.selectors';
 
 @Injectable({
@@ -9,12 +10,25 @@ import * as CarriageSelectors from './carriage.selectors';
 })
 export class CarriageFacade {
   carriages$ = this.store.select(CarriageSelectors.selectCarriages);
-  carriageError$ = this.store.select(CarriageSelectors.selectError);
   isLoading$ = this.store.select(CarriageSelectors.selectIsLoading);
+  carriageError$ = this.store.select(CarriageSelectors.selectError);
+  deleteError$ = this.store.select(CarriageSelectors.selectDeleteError);
 
   constructor(private store: Store) {}
 
   loadCarriages(): void {
     this.store.dispatch(CarriageActions.loadCarriages());
+  }
+
+  createCarriage(carriage: Carriage): void {
+    this.store.dispatch(CarriageActions.createCarriage({ carriage }));
+  }
+
+  updateCarriage(carriage: Carriage): void {
+    this.store.dispatch(CarriageActions.updateCarriage({ carriage }));
+  }
+
+  deleteCarriage(code: string): void {
+    this.store.dispatch(CarriageActions.deleteCarriage({ code }));
   }
 }
