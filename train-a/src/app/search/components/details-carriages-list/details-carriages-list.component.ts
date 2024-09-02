28143e5490ba9ceffff3bff04d48b5ff8fa03bec
FYMG <svinpetr18@gmail.com>
2024-09-03 00:39:08 +0300

    feat: add update after success

--- train-a/src/app/search/components/details-carriages-list/details-carriages-list.component.ts
+++ train-a/src/app/search/components/details-carriages-list/details-carriages-list.component.ts
@@ -1,5 +1,5 @@
 import { NgIf } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
 
 import { CarriageCardComponent } from '../../../carriages/components/carriage-card/carriage-card.component';
 import { Carriage } from '../../../carriages/state/carriage.model';
@@ -21,13 +21,17 @@ export interface SelectSeatOutput {
   templateUrl: './details-carriages-list.component.html',
   styleUrl: './details-carriages-list.component.scss',
 })
-export class DetailsCarriagesListComponent implements OnInit {
+export class DetailsCarriagesListComponent implements OnInit, OnChanges {
   @Input() details!: RideDetails;
   @Input() carriages!: Carriage[];
   @Input() carriageTypeIndex: number = 0;
   @Output() selectSeat = new EventEmitter<SelectSeatOutput>();
   selectedSeat?: number;
   carriagesMeta!: ReturnType<typeof getCarriageSeats>;
+
+  ngOnChanges() {
+    this.carriagesMeta = getCarriageSeats(this.details, this.carriages);
+  }
 
   ngOnInit() {
     this.carriagesMeta = getCarriageSeats(this.details, this.carriages);
