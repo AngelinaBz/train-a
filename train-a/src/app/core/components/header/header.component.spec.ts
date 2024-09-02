f6761122d2893c44c0d8869141360e494bd1a325
v-kuntsevich <kwakin@gmail.com>
2024-09-02 17:51:44 +0300

    feat: add header.comp.spec

--- train-a/src/app/core/components/header/header.component.spec.ts
+++ train-a/src/app/core/components/header/header.component.spec.ts
@@ -1,22 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
 
+import { UserFacade } from '../../../user/state/user.facade';
+import { userReducer } from '../../../user/state/user.reducers';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeaderComponent],
+      imports: [StoreModule.forRoot({ user: userReducer })],
+      declarations: [HeaderComponent],
+      providers: [UserFacade],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
