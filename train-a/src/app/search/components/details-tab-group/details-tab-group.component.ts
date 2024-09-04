d0d58716640306daacad559856c7cc5d12992004
FYM <34478249+FYMG@users.noreply.github.com>
2024-09-04 22:59:38 +0300

    Fix Details page (#43)

--- train-a/src/app/search/components/details-tab-group/details-tab-group.component.ts
+++ train-a/src/app/search/components/details-tab-group/details-tab-group.component.ts
@@ -17,12 +17,27 @@ import RideDetails from '../../models/RideDetails.model';
 export class DetailsTabGroupComponent {
   @Input() details!: RideDetails;
   @Input() carriages!: Carriage[];
-  @Input() activeTabIndex: number = 0;
   @Output() tabChange = new EventEmitter<number>();
   protected readonly getAllRideSeats = getAllRideSeats;
   protected readonly getAllRidePrices = getAllRidePrices;
 
   onTabChange(event: MatTabChangeEvent) {
-    this.tabChange.emit(event.index);
+    const list = this.carriages;
+    const filteredList = this.carriages.filter((carriage) => this.details.carriages.includes(carriage.code));
+
+    const result = list.reduce((acc: Record<number, number | null>, item, i) => {
+      acc[i] = filteredList.includes(item) ? filteredList.findIndex((carriage) => carriage.code === item.code) : null;
+      return acc;
+    }, {});
+
+    const swappedObject = Object.entries(result).reduce((acc: Record<number, number>, [key, value]) => {
+      if (value == null) {
+        return acc;
+      }
+      acc[Number(value)] = Number(key);
+      return acc;
+    }, {});
+
+    this.tabChange.emit(swappedObject[event.index]);
   }
 }
